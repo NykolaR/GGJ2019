23b7fbec80f1c9e6c6bf37e80a034a1a70d82fb6
N <thenykreed@gmail.com>
2019-01-26 12:54:19 -0600

    house factory

--- Scenes/Buildings/houses.gd
+++ Scenes/Buildings/houses.gd
@@ -5,3 +5,9 @@ func _ready():
 
 func get_random_house() -> Node:
 	return get_child(0).duplicate(DUPLICATE_USE_INSTANCING)
+
+func get_residential_house() -> Node:
+	return get_child(0).duplicate(DUPLICATE_USE_INSTANCING)
+
+func get_skyscraper_house() -> Node:
+	return get_child(1).duplicate(DUPLICATE_USE_INSTANCING)
