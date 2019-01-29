4983627c7e1004f24182ce97cbe56e7aa6568cf0
N <thenykreed@gmail.com>
2019-01-29 11:46:26 -0600

    fixed explanation scene

--- Scenes/Levels/Explanation.gd
+++ Scenes/Levels/Explanation.gd
@@ -1,14 +1,18 @@
 extends Spatial
 
-#onready var dir_controls
+onready var tank = $VBox/HBox/tank
+onready var dir = $VBox/HBox/dir
 
 func _process(delta):
-	#if (Settings.tank_controls):
-	#	get_node("CanvasLayer/Control/TextureRect2").visible = false;
-	#	get_node("CanvasLayer/Control/TextureRect3").visible = true;
-	#else:
-	#	get_node("CanvasLayer/Control/TextureRect2").visible = true;
-	#	get_node("CanvasLayer/Control/TextureRect3").visible = false;
+	# is probably overkill/inefficient to check every frame
+	# but also, probably doesn't really matter
+	
+	if Settings.tank_controls:
+		tank.self_modulate = Settings.GREEN
+		dir.self_modulate = Settings.RED
+	else:
+		dir.self_modulate = Settings.GREEN
+		tank.self_modulate = Settings.RED
 	
 	if (Input.is_action_just_pressed("ui_accept")):
 		get_tree().change_scene("res://Scenes/Levels/Game.tscn")
